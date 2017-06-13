8cc1719a0008b9087405b269de1463f9ff7ca1a1
LXY02 <1973786292@qq.com>
2017-06-13 11:58:55 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
 	<div id="app">
 		<section class="wrap js_warp">
-			<mod_top></mod_top>
-			<mod_hit></mod_hit>
-			<mod_vote></mod_vote>
-			<mod_rank></mod_rank>
-			<mod_playlist></mod_playlist>
-			<mod_friendlink></mod_friendlink>
-			<mod_comment></mod_comment>
+			<!--<mod_top></mod_top>-->
+			<!--<mod_hit></mod_hit>-->
+			<mod-vote></mod-vote>
+			<!--<mod_rank></mod_rank>-->
+			<!--<mod_playlist></mod_playlist>-->
+			<!--<mod_friendlink></mod_friendlink>-->
+			<!--<mod_comment></mod_comment>-->
 		</section>
 	</div>
 </template>
 
 <script>
-	import modTop from './mod_top.vue'
-	import modHit from './mod_hit.vue'
+//	import modTop from './mod_top.vue'
+//	import modHit from './mod_hit.vue'
 	import modVote from './mod_vote.vue'
-	import modRank from './mod_rank.vue'
-	import modPlaylist from './mod_playlist.vue'
-	import modFriendlink from './mod_friendlink.vue'
-	import modComment from './mod_comment.vue'
+//	import modRank from './mod_rank.vue'
+//	import modPlaylist from './mod_playlist.vue'
+//	import modFriendlink from './mod_friendlink.vue'
+//	import modComment from './mod_comment.vue'
 	export default {
 		name: 'app',
 		data () {
@@ -28,13 +28,13 @@
 			}
 		},
 		components: {
-			modTop,
-			modHit,
-			modVote,
-			modRank,
-			modPlaylist,
-			modFriendlink,
-			modComment
+//			modTop,
+//			modHit,
+			modVote
+//			modRank,
+//			modPlaylist,
+//			modFriendlink,
+//			modComment
 		}
 	}
 </script>
--- src/mod_vote.vue
+++ src/mod_vote.vue
@@ -1,16 +1,97 @@
 <template>
+    <article class="mod_vote c_txt1">
+        <header class="mod_tit c_txt1">
+            <h2 class="tit__txt">《金曲捞》开场曲由你定！</h2>
+            <p class="tit__desc">投票截止时间：5月28日24点</p>
+        </header>
+        <nav class="vote__nav c_txt1">
+            <div class="vote__nav_box js_vote_navbox" style="transform: translateX(20px);">
+                <a href="javascript:;" class="vote__nav_item js_vote_nav cur c_txt3" data-endtime="2017-05-28 23:50:00" data-end="1" data-index="0" data-voteset="94">第十一期</a>
+                <a href="javascript:;" class="vote__nav_item js_vote_nav c_txt1" data-endtime="2017-05-22 23:00:00" data-end="1" data-index="1" data-voteset="93">第十期</a>
+                <a href="javascript:;" class="vote__nav_item js_vote_nav c_txt1" data-endtime="2017-05-14 23:59:59" data-end="1" data-index="2" data-voteset="92">第九期</a>
+                <a href="javascript:;" class="vote__nav_item js_vote_nav c_txt1" data-endtime="2017-05-08 23:59:59" data-end="1" data-index="3" data-voteset="91">第八期</a>
+                <a href="javascript:;" class="vote__nav_item js_vote_nav c_txt1" data-endtime="2017-05-02 23:59:59" data-end="1" data-index="4" data-voteset="90">第七期</a>
+            </div>
+        </nav>
+        <section class="vote__bd js_vote_wrap">
+            <div class="vote__bd_box js_vote_wrapbox cur" data-index="0" data-voteset="94" style="transform: translateX(0%);">
+                <ul class="vote_list js_vote_list" data-voteset="94">
+                    <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="94001" data-voteset="94">
+                        <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="94001" data-voteset="94" style="display: none;"></a>
+                            <span class="vote_list__txt c_txt1">范晓萱《Tell Her》</span>
+                            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="133020">
+                                <svg class="icon_svg c_txt1 icon_play_r">
+                                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                                </svg>
+                            </a>
+                            <span class="vote_list__count vote_list__count--top c_txt3">40021票</span>
+                    </li>
+                </ul>
+                <a href="javascript:;" class="vote__btn c_txt1 disable" data-voteset="94">已结束</a>
+            </div>
+
+            <div class="vote__bd_box js_vote_wrapbox " data-index="1" data-voteset="93" style="transform: translateX(0%);"></div>
+
+            <div class="vote__bd_box js_vote_wrapbox " data-index="2" data-voteset="92" style="transform: translateX(0%);"></div>
+
+            <div class="vote__bd_box js_vote_wrapbox" data-index="3" data-voteset="91" style="transform: translateX(0%);">     <ul class="vote_list js_vote_list" data-voteset="91">          <!--  -->     <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="91003" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91003" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">动物世界</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="201452010">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count vote_list__count--top c_txt3">21万票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="91007" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91007" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">暧昧</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="201697348">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count vote_list__count--top c_txt3">20万票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="91004" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91004" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">我害怕</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="201974489">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count vote_list__count--top c_txt3">79962票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91008" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91008" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">高尚</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="201213781">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">78673票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91010" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91010" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">一半</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="104792679">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">70142票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91009" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91009" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">其实</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="104775877">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">60347票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91005" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91005" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">下雨了</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="102636800">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">51707票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91001" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91001" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">未完成的歌</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="647946">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">41383票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91002" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91002" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">你过得好吗</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="1308680">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">36716票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="91006" data-voteset="91">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="91006" data-voteset="91" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">小孩</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="104674830">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">36397票</span>     </li>    <!--  -->           </ul>     <a href="javascript:;" class="vote__btn c_txt1 disable" data-voteset="91">已结束</a> </div>
+
+            <div class="vote__bd_box js_vote_wrapbox" data-index="4" data-voteset="90" style="transform: translateX(0%);">     <ul class="vote_list js_vote_list" data-voteset="90">          <!--  -->     <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="90001" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90001" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">伍思凯《爱与愁》</span>            <span class="vote_list__count vote_list__count--top c_txt3">40284票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="90010" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90010" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">陈慧琳《是我不好》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="9826">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count vote_list__count--top c_txt3">39042票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item vote_list__count--top c_txt3" data-id="90009" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90009" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">蔡健雅《伸伸懒腰》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="7242283">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count vote_list__count--top c_txt3">34285票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90008" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90008" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">曹格《爱转动》</span>            <span class="vote_list__count c_txt1">31529票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90006" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90006" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">动力火车《单程车票》</span>            <span class="vote_list__count c_txt1">27993票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90004" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90004" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">老狼《昨天今天》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="102346650">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">27286票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90003" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90003" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">黄绮珊《爱是什么》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="105419532">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">24666票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90002" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90002" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">罗大佑《传说》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="7230885">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">17722票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90005" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90005" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">阿杜《一个人住》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="101458">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">14425票</span>     </li>    <!--  -->             <!--  -->     <li class="js_vote_item vote_list__item" data-id="90007" data-voteset="90">                                          <a href="javascript:;" class="vote_list__check c_txt1 js_vote_sel " data-id="90007" data-voteset="90" style="display: none;"></a>                           <span class="vote_list__txt c_txt1">崔健《不是我不明白》</span>            <a href="javasript:;" class="vote_list__play c_txt1 js_playsong_btn" title="播放" data-id="2464725">       <svg class="icon_svg c_txt1 icon_play_r">        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>       </svg>      </a>            <span class="vote_list__count c_txt1">12754票</span>     </li>    <!--  -->           </ul>     <a href="javascript:;" class="vote__btn c_txt1 disable" data-voteset="90">已结束</a> </div>
+
+        </section>
+    </article>
 </template>
 
 <script>
     export default {
         data () {
             return {
-                name: 'lalalal',
-                age: 1234
+
             }
         },
     }
 </script>
 
 <style>
+    .c_txt1 {
+        color: #2b2322;
+    }
+    .mod_vote {
+        position: relative;
+        margin: 0 10px 36px;
+    }
+    .mod_vote::before {
+        left: 0;
+        border-right: none;
+    }
+    .mod_vote::after, .mod_vote::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        width: 41px;
+        height: 100%;
+        border: solid 8px #000;
+        border-color: currentColor;
+        -webkit-box-sizing: border-box;
+        opacity: .05;
+        background-color: transparent;
+    }
+    .mod_vote::after {
+        right: 0;
+        border-left: none;
+    }
+    .mod_tit {
+        margin-bottom: 14px;
+        color: #000;
+        text-align: center;
+        line-height: 1;
+    }
+    .tit__txt {
+        font-size: 20px;
+        font-weight: 400;
+    }
+    .tit__desc {
+        margin-top: 6px;
+        font-size: 16px;
+    }
 </style>
