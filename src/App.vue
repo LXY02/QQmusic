6fda950c4eabfc26f48b5c077ae06451ca89be4e
zhishaofei <79915957@qq.com>
2017-06-13 10:59:29 +0800

    金曲捞 主页面引入其他模块

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
 	<div id="app">
-
+		<section class="wrap js_warp">
+			<mod_top></mod_top>
+			<mod_hit></mod_hit>
+			<mod_vote></mod_vote>
+			<mod_rank></mod_rank>
+			<mod_playlist></mod_playlist>
+			<mod_friendlink></mod_friendlink>
+			<mod_comment></mod_comment>
+		</section>
 	</div>
 </template>
 
 <script>
+	import modTop from './mod_top.vue'
+	import modHit from './mod_hit.vue'
+	import modVote from './mod_vote.vue'
+	import modRank from './mod_rank.vue'
+	import modPlaylist from './mod_playlist.vue'
+	import modFriendlink from './mod_friendlink.vue'
+	import modComment from './mod_comment.vue'
 	export default {
 		name: 'app',
 		data () {
 			return {
 
 			}
+		},
+		components: {
+			modTop,
+			modHit,
+			modVote,
+			modRank,
+			modPlaylist,
+			modFriendlink,
+			modComment
 		}
 	}
 </script>
