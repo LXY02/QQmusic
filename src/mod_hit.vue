7c6c48bfdab6ba833b1e94dd9ffb7ce378857e8e
sunxt1989 <526028139@qq.com>
2017-06-13 11:30:50 +0800

    创建 mod_hit ul列表

--- src/mod_hit.vue
+++ src/mod_hit.vue
@@ -1,5 +1,278 @@
 <template>
-    <section class="mod_hit"></section>
+    <section class="mod_hit">
+        <ul class="hit_list hit_list--special js_song_list" data-songids="202715897,202715902,202715903,202715900,202715901,202715898,202715899">
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715898" data-mid="003Y6iQH23fbk8">
+            <span class="hit_list__index c_txt3 js_order_index">1</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108416.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">一样的月光</h3>
+                        <p class="hit_list__desc c_txt1">金志文</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715898" data-mid="003Y6iQH23fbk8" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715898" data-num="149583">149583票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715898">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715898">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="一样的月光-金志文" data-songid="202715898" data-desc="一样的月光">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715902" data-mid="004H79oC0u54f9">
+            <span class="hit_list__index c_txt3 js_order_index">2</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108412.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">空窗</h3>
+                        <p class="hit_list__desc c_txt1">A-Lin</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715902" data-mid="004H79oC0u54f9" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715902" data-num="110858">110858票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715902">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715902">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="空窗-A-Lin" data-songid="202715902" data-desc="空窗">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715897" data-mid="003zIjTB1MZQsY">
+            <span class="hit_list__index c_txt3 js_order_index">3</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108411.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">我不睡</h3>
+                        <p class="hit_list__desc c_txt1">黄国伦</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715897" data-mid="003zIjTB1MZQsY" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715897" data-num="48329">48329票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715897">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715897">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="我不睡-黄国伦" data-songid="202715897" data-desc="我不睡">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715901" data-mid="002Evnyc4RM0l2">
+            <span class="hit_list__index c_txt3 js_order_index">4</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108415.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">爱得太多</h3>
+                        <p class="hit_list__desc c_txt1">王杰 / 黄绮珊</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715901" data-mid="002Evnyc4RM0l2" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715901" data-num="36035">36035票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715901">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715901">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="爱得太多-王杰" data-songid="202715901" data-desc="爱得太多">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715899" data-mid="000qjU3Z0HB7QW">
+            <span class="hit_list__index c_txt3 js_order_index">5</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108417.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">一样的月光</h3>
+                        <p class="hit_list__desc c_txt1">徐佳莹 / 金志文</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715899" data-mid="000qjU3Z0HB7QW" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715899" data-num="25071">25071票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715899">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715899">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="一样的月光-徐佳莹" data-songid="202715899" data-desc="一样的月光">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715903" data-mid="001tYNmX3q7r3e">
+            <span class="hit_list__index c_txt3 js_order_index">6</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108413.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">空窗</h3>
+                        <p class="hit_list__desc c_txt1">辛晓琪 / A-Lin</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715903" data-mid="001tYNmX3q7r3e" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715903" data-num="12820">12820票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715903">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715903">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="空窗-辛晓琪" data-songid="202715903" data-desc="空窗">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+            <li class="hit_list__item c_bor1 js_curlist_song js_playsongcon" data-id="202715900" data-mid="001qI5qE3Ot6ye">
+            <span class="hit_list__index c_txt3 js_order_index">7</span>
+            <a class="hit_list__avatar">
+                <img src="//y.gtimg.cn/music/common/upload/t_tv_songShow_songs/108414.jpg?max_age=2592000" class="hit_list__avatar_img" alt="">
+            </a>
+            <div class="hit_list__bd">
+                <div class="hit_list__info">
+                    <div class="hit_list__txt">
+                        <h3 class="hit_list__tit c_txt1">爱得太多</h3>
+                        <p class="hit_list__desc c_txt1">黄绮珊</p>
+                    </div>
+
+                    <div class="hit_list__other js_hit_div">
+                        <a href="javascript:;" class="hit_list__btn c_txt1 js_hitbtn" data-voted="0" data-id="202715900" data-mid="001qI5qE3Ot6ye" data-no="509">投票</a>
+                        <span class="hit_list__count c_txt1 js_vote_num" data-id="202715900" data-num="9831">9831票</span>
+                    </div>
+
+                </div>
+                <div class="hit_list__control">
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_playsong" title="播放" data-id="202715900">
+                        <svg class="icon_svg c_txt1 icon_play_r">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                        </svg>
+                    </a>
+
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_downsong" title="下载" data-id="202715900">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;" class="hit_list__control_item c_txt1 js_share" title="分享" data-title="爱得太多-黄绮珊" data-songid="202715900" data-desc="爱得太多">
+                        <svg class="icon_svg c_txt1">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#share"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </li>
+        </ul>
+        <div class="hit__ft">
+            <a href="javascript:;" class="hit__more c_txt1 js_dlg_show" data-dlg="toprule" data-tj="showhotrule">打榜规则 &gt;</a>
+            <a href="javascript:;" class="hit__more c_txt1 js_showcalendar">往期回顾 &gt;</a>
+        </div>
+    </section>
 </template>
 
 <script>
