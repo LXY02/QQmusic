7e5b203eb6472450c3bf0617bc9bd14c6f21bea8
zhishaofei <79915957@qq.com>
2017-06-13 11:03:44 +0800

    主页面样式

--- src/App.vue
+++ src/App.vue
@@ -40,5 +40,19 @@
 </script>
 
 <style>
+	/*自由配置项_S*/
+	.c_txt3{color:#2b2321;}/*高亮字色、镂空按钮色*/
+	.c_bg1{background-color:#f1ece6;}/*页面背景色*/
+	/*自由配置项_E*/
 
+
+	/*浅底配色_S*/
+	.c_txt1{color:#2b2322;}/*常应用于正文字色*/
+	.c_bor1,
+	.c_bor1::after{border-color:rgba(43,35,34,.1);}
+	.c_bg2{background-color:rgba(0,0,0,.1);}/*常应用于内容块底色*/
+	/*浅底配色_E*/
+
+
+	.popup_history__cont{-webkit-overflow-scrolling: touch;margin-top: 32px;overflow: scroll;overflow-x: hidden;height: 100%}
 </style>
